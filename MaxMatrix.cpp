d5f8f3f3ced391fb76502cb158aac49fd6ecdb04
Gefrierbrand <nineff@gmail.com>
2013-10-12 17:14:56 +0200

    initial upload

---
+++ MaxMatrix.cpp
@@ -0,0 +1,117 @@
+/* MaxMatrix2
+ * Version 0.1 OCT 2013
+ * Copyright 2013 Nikolai Neff
+ * based upon MaxMatrix by Oscar Kin-Chung Au	
+ * 
+ *
+ * This file is part of MaxMatrix2
+ * 
+ * MaxMatrix2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MaxMatrix2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MaxMatrix2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#include "Arduino.h"
+#include "MaxMatrix.h"
+
+MaxMatrix::MaxMatrix(byte _data, byte _load, byte _clock, byte _numDisplays) 
+{
+	data = _data;
+	load = _load;
+	clock = _clock;
+	numDisplays = _numDisplays;
+}
+
+void MaxMatrix::init(byte scanLimit, byte decodeMode, bool displayTest)
+{
+	pinMode(data,  OUTPUT);
+	pinMode(clock, OUTPUT);
+	pinMode(load,  OUTPUT);
+	
+	/* From the MAX7219/MAX7221 Datasheet:
+	On initial power-up, all control registers are reset, the
+	display is blanked, and the MAX7219/MAX7221 enter
+	shutdown mode. Program the display driver prior to
+	display use. Otherwise, it will initially be set to scan one
+	digit, it will not decode data in the data registers, and
+	the intensity register will be set to its minimum value.
+	*/
+	
+	
+	//digitalWrite(clock, HIGH); digitalWrite(load, HIGH);
+	
+	for (byte i=0; i<numDisplays; i++) //todo: make this efficient (don't send to each display individually)
+	{
+		sendCommand(i, max7219_reg_scanLimit, scanLimit);      
+		sendCommand(i, max7219_reg_decodeMode, decodeMode);
+		sendCommand(i, max7219_reg_shutdown, 0x01);    //Activate display
+		
+		if (displayTest == true)
+		{
+			sendCommand(i, max7219_reg_displayTest, 0x01); //displayTest defaults to false, so only send if needed
+		}	
+	}
+}
+
+void MaxMatrix::setIntensity(byte display, byte intensity) //sets the intensity for a specific display
+{
+	sendCommand(display, max7219_reg_intensity, intensity);
+}
+
+void MaxMatrix::setDecodeMode(byte display, byte decodeMode) //sets the decoding for CODE-B Font for specific digits for a specific display
+{
+	sendCommand(display, max7219_reg_decodeMode, decodeMode);
+}
+
+void MaxMatrix::setShutdown(byte display, byte shutdown) //shutdown or wake up specific display
+{
+	sendCommand(display, max7219_reg_shutdown, shutdown);
+}
+
+void MaxMatrix::setTestMode(byte display, byte testMode) //test specific display
+{
+	sendCommand(display, max7219_reg_displayTest, testMode);
+}
+
+void MaxMatrix::clear(byte display)
+{
+	for (byte i=1; i<=8; i++) //output registers are 1 to 8
+	{
+		sendCommand(display, i, 0); //set all outputs to 0
+	}
+}
+
+void MaxMatrix::clearAll()
+{
+	//todo: implement
+}
+
+void MaxMatrix::sendCommand(byte display, byte registerAddr, byte value) // sends a command to a specific Display
+{
+	digitalWrite(load, LOW); //indicates data transfer start
+	
+	shiftOut(data, clock, MSBFIRST, registerAddr); //send desired 16 bit of data split up in to parts, as Arduino only supports 8 bit in software-SPI mode
+	shiftOut(data, clock, MSBFIRST, value);
+	
+	for (byte i=0; i<display-1; i++) //send Display -1 No-Op Commands to address the right display. See Section "No-Op Register" in the Max 7219 datasheet for details
+	{
+		shiftOut(data, clock, MSBFIRST, max7219_reg_noop);
+		shiftOut(data, clock, MSBFIRST, 0x00);
+	}
+	
+	digitalWrite(load, HIGH); //indicates data transfer end
+}
+
+void MaxMatrix::sendArray(byte buffer[8],byte num)
+{
+	//todo: Implement me
+}
